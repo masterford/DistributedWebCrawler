4d43bdd569e8793983ee20e15a68856e46e06bfc
Ransford Antwi <ransford@seas.upenn.edu>
2020-05-08 12:53:18 -0400

    write to file

---
+++ Crawler/src/main/java/edu/upenn/cis455/storage/WriteToFile.java
@@ -0,0 +1,20 @@
+package edu.upenn.cis455.storage;
+
+public class WriteToFile {
+
+	public static void main(String[] args) {
+		String [] alphabet = {"A", "B", "C", "D", "E"};
+		try {
+			String directory = System.getProperty("user.dir")+ "/DistributedStorage/" + args[0];
+			StorageServer.getInstance().init(directory);
+			int index = Integer.parseInt(args[0].split("_")[1]);
+			StorageServer.getInstance().writetoFile("corpus" + alphabet[index] + "_0");
+		}catch (NumberFormatException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}finally {
+			System.exit(0);
+		}
+	}
+
+}
